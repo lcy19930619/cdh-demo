2f0966e4e5411c71ee05333aae8467c66203e897
冷春阳 <lcy19930619@163.com>
2023-04-12 11:24:37 +0800

    init

---
+++ src/main/java/com/example/cdh/CdhDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdhDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdhDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cdh/CdhDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cdh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdhDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
