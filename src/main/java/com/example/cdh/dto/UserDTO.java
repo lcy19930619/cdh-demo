cf18b45e51ccf1e83831232b79fe430933ca1d88
冷春阳 <lcy19930619@163.com>
2023-04-18 11:00:46 +0800

    代码完善，使用自定义数据类型解析

---
+++ src/main/java/com/example/cdh/dto/UserDTO.java
@@ -0,0 +1,27 @@
+package com.example.cdh.dto;
+
+/**
+ * @author chunyang.leng
+ * @date 2023-04-18 10:55
+ */
+public class UserDTO {
+    private String name;
+
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
--- src/main/java/com/example/cdh/service/SparkOfflineService.java
+++ src/main/java/com/example/cdh/service/SparkOfflineService.java
@@ -1,10 +1,11 @@
 package com.example.cdh.service;
 
+import com.example.cdh.dto.UserDTO;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.function.VoidFunction;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
+import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.DataTypes;
 import org.apache.spark.sql.types.StructField;
@@ -15,6 +16,7 @@ import scala.Serializable;
 
 /**
  * spark 离线计算
+ *
  * @author chunyang.leng
  * @date 2023-04-12 14:53
  */
@@ -25,38 +27,48 @@ public class SparkOfflineService implements Serializable {
 
     /**
      * 统计 hdfs 中一个csv文件的行数
+     *
      * @param hdfsPath demo: hdfs://cdh-slave-1:8020/demo/csv/input.csv
      * @return
      */
-    public long countHdfsCsv(String hdfsPath){
+    public long countHdfsCsv(String hdfsPath) {
         return sparkSession.read().csv(hdfsPath).count();
     }
 
-
-    public long filterHdfsCsvLteAge(String hdfsPath,int age) throws AnalysisException {
+    public long filterHdfsCsvLteAge(String hdfsPath, int age) throws AnalysisException {
         // 临时表名称
-        String tempTableName =  "cdh_demo_input_csv";
+        String tempTableName = "cdh_demo_input_csv";
 
+        // 定义数据类型
         StructField nameField = DataTypes.createStructField("name", DataTypes.StringType, true);
         StructField ageField = DataTypes.createStructField("age", DataTypes.IntegerType, true);
 
         StructField[] fields = new StructField[2];
         fields[0] = nameField;
         fields[1] = ageField;
-        StructType type = new StructType(fields);
+        StructType schema = new StructType(fields);
 
-        Dataset<Row> csv = sparkSession
+        Dataset<UserDTO> csv = sparkSession
             .read()
-            .schema(type)
-            .csv(hdfsPath);
+            .schema(schema)
+            .csv(hdfsPath)
+            .map(row -> {
+                UserDTO dto = new UserDTO();
+                dto.setName(row.getString(0));
+                dto.setAge(row.getInt(1));
+                return dto;
+            }, Encoders.bean(UserDTO.class));
+
         csv.createOrReplaceTempView(tempTableName);
 
-        Dataset<Row> sql = sparkSession
-            .sql("select * from " + tempTableName + " where age <= " + age);
-        JavaRDD<Row> rdd = sql.toJavaRDD();
-        rdd.foreach(new VoidFunction<Row>() {
-            @Override public void call(Row row) throws Exception {
-                System.out.println(row.getString(0));
+        Dataset<UserDTO> sql = sparkSession
+            .sql("select * from " + tempTableName + " where age <= " + age)
+            .as(Encoders.bean(UserDTO.class));
+        JavaRDD<UserDTO> rdd = sql.toJavaRDD();
+        rdd.foreach(new VoidFunction<UserDTO>() {
+            @Override
+            public void call(UserDTO dto) throws Exception {
+                System.out.println(dto.getName() + " <====> " +dto.getAge());
             }
         });
         return csv.count();
